16d4b471317e225a2b8ebd05877e9ed8d29119d1
Chris Rude <chrisrude@users.noreply.github.com>
2023-05-04 16:31:45 -0700

    show warning if persona prompt too long

--- src/oobabot/discord_bot.py
+++ src/oobabot/discord_bot.py
@@ -46,30 +46,37 @@ def sanitize_message(raw_message: discord.Message) -> dict[str, str]:
 
 class PromptGenerator:
     '''
-    Purpose: generate a prompt for the AI to use, given
+    Purpose: generate a prompt_prefix for the AI to use, given
     the message history and persona.
     '''
 
     # this is set by the AI, and is the maximum length
     # it will understand before it starts to ignore
-    # the rest of the prompt
-    MAX_PROMPT_LEN = 2048
+    # the rest of the prompt_prefix
+    MAX_AI_TOKEN_SPACE = 2048
+
+    # our structure will be:
+    # <prompt_prefix (includes persona)>
+    # <a bunch of history lines>
+    # <ai response>
+    #
+    # figure out our budget for the persona and history
+    # so that we can be sure to always have at least
+    # RESERVED_FOR_AI_RESPONSE tokens for the AI to use
+
+    HIST_SIZE = 10
+    EST_CHARS_PER_HISTORY_LINE = 40
+    EXPECTED_HISTORY_SIZE = HIST_SIZE * EST_CHARS_PER_HISTORY_LINE
+
+    # this is the number of tokens we reserve for the AI
+    # to respond with.
+    RESERVED_FOR_AI_RESPONSE = 512
 
     def __init__(self, ai_name: str,
                  ai_persona: str):
         self.ai_name = ai_name
         self.ai_persona = ai_persona
-
-    async def generate_prompt(
-            self,
-            ai_user_id: int,
-            message_history: typing.AsyncIterator[discord.Message]) \
-            -> str:
-        '''
-        Generates a prompt for the AI to use based on the message.
-        '''
-
-        prompt = textwrap.dedent(f'''
+        self.prompt_prefix = textwrap.dedent(f'''
         You are in a chat room with multiple participants.
         Below is a transcript of recent messages in the conversation.
         Write the next message that you would send in this conversation,
@@ -77,16 +84,33 @@ class PromptGenerator:
         Here is some background information about "{self.ai_name}":
         {self.ai_persona}
         All responses you write must be from the point of view of
-        {self.ai_name}, and plausible for {self.ai_name} to say in
-        this conversation.  Do not continue the conversation as
-        anyone other than {self.ai_name}.
+        {self.ai_name}.
         ### Transcript:
         ''')
+        prompt_prefix_len = len(self.prompt_prefix)
+        prompt_prefix_len += self.EXPECTED_HISTORY_SIZE
+        free_for_ai_response = self.MAX_AI_TOKEN_SPACE - prompt_prefix_len
+        if free_for_ai_response < self.RESERVED_FOR_AI_RESPONSE:
+            raise ValueError(
+                'AI token space is too small for prompt_prefix and history. ' +
+                'Please shorten your persona by ' +
+                f'{self.RESERVED_FOR_AI_RESPONSE - free_for_ai_response} ' +
+                'characters.'
+            )
+
+    async def generate(
+            self,
+            ai_user_id: int,
+            message_history: typing.AsyncIterator[discord.Message]) \
+            -> str:
+        '''
+        Generates a prompt_prefix for the AI to use based on the message.
+        '''
 
         # put this at the very end to tell the UI what it
         # should complete.  But generate this now so we
         # know how long it is.
-        prompt_footer = f'{self.ai_name} says:\n'
+        prompt_prefix_footer = f'{self.ai_name} says:\n'
 
         # add on more history, but only if we have room
         # if we don't have room, we'll just truncate the history
@@ -94,13 +118,14 @@ class PromptGenerator:
         # this is s
         # it will understand before ignore
         #
-        prompt_len_remaining = self.MAX_PROMPT_LEN - \
-            len(prompt) - len(prompt_footer)
+        prompt_len_remaining = self.MAX_AI_TOKEN_SPACE
+        prompt_len_remaining -= len(self.prompt_prefix)
+        prompt_len_remaining -= len(prompt_prefix_footer)
+        prompt_len_remaining -= self.RESERVED_FOR_AI_RESPONSE
 
         # history_lines is newest first, so figure out
         # how many we can take, then append them in
         # reverse order
-        added_history = 0
         history_lines = []
         async for raw_message in message_history:
             clean_message = sanitize_message(raw_message)
@@ -117,26 +142,24 @@ class PromptGenerator:
 
             if len(line) > prompt_len_remaining:
                 get_logger().warn(
-                    'prompt too long, truncating history.  ' +
-                    f'added {added_history} lines, dropped ' +
-                    f'{len(history_lines)} lines'
+                    'ran out of space, only including ' +
+                    f'{len(history_lines)} history lines'
                 )
                 break
 
             prompt_len_remaining -= len(line)
             history_lines.append(line)
-            added_history += 1
 
         history_lines.reverse()
-        prompt += ''.join(history_lines)
 
-        prompt += prompt_footer
+        prompt = self.prompt_prefix
+        prompt += ''.join(history_lines)
+        prompt += prompt_prefix_footer
 
         return prompt
 
 
 class DiscordBot(discord.Client):
-    HIST_SIZE = 10
 
     def __init__(self,
                  ooba_client: OobaClient,
@@ -153,7 +176,8 @@ class DiscordBot(discord.Client):
         self.log_all_the_things = log_all_the_things
 
         self.average_stats = AggregateResponseStats(ooba_client)
-        self.prompt_generator = PromptGenerator(ai_name, ai_persona)
+        self.prompt_prefix_generator = PromptGenerator(
+            ai_name, ai_persona)
 
         # match messages that include any `wakeword`, but not as part of
         # another word
@@ -237,20 +261,21 @@ class DiscordBot(discord.Client):
         get_logger().debug(
             f'Request from {author} in server [{server}]')
 
-        recent_messages = raw_message.channel.history(limit=self.HIST_SIZE)
+        recent_messages = raw_message.channel.history(
+            limit=PromptGenerator.HIST_SIZE)
 
-        prompt = await self.prompt_generator.generate_prompt(
+        prompt_prefix = await self.prompt_prefix_generator.generate(
             self.ai_user_id, recent_messages)
 
-        response_stats = self.average_stats.log_request_arrived(prompt)
+        response_stats = self.average_stats.log_request_arrived(prompt_prefix)
         if self.log_all_the_things:
-            print('Prompt:\n----------\n')
-            print(prompt)
+            print('prompt_prefix:\n----------\n')
+            print(prompt_prefix)
             print('Response:\n----------\n')
 
         try:
             async for sentence in self.ooba_client.request_by_sentence(
-                prompt
+                prompt_prefix
             ):
                 if self.log_all_the_things:
                     print(sentence)
--- src/oobabot/response_stats.py
+++ src/oobabot/response_stats.py
@@ -190,7 +190,7 @@ class AggregateResponseStats:
             )
 
         get_logger().debug(
-            'Prompt length:               ' +
+            'Prompt length: ' +
             f'max: {self.prompt_max_chars}, ' +
             f'min: {self.prompt_min_chars}, ' +
             f'avg: {self.average_prompt_length():.2f}'
